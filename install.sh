8f30dc8fbd503cdcfae936b12116fc54c5b2418d
Peter Todorov <peter.todorov@live.com>
2021-05-14 09:10:23 +0300

    Add first version of installed software

---
+++ install.sh
@@ -0,0 +1,10 @@
+cd ~/Downloads/
+sudo snap install --classic code
+sudo apt install latexmk
+sudo apt-get install texlive-full
+sudo apt install firefox
+sudo apt install google-chrome-stable
+sudo snap install gimp
+sudo apt install vlc
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt update
