74a6724b307485a811d5cf0561da2aca84c8cad4
Peter Todorov <peter.todorov@live.com>
2021-10-26 23:33:39 +0200

    Add git and htop to installation

--- install.sh
+++ install.sh
@@ -6,5 +6,7 @@ sudo apt install firefox
 sudo apt install google-chrome-stable
 sudo snap install gimp
 sudo apt install vlc
+sudo apt install htop
+sudo apt install git
 sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt update
