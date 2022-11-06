05ecb452d36afb959bb935dd892513a184027ce9
Peter Todorov <peter.todorov@live.com>
2022-11-06 14:35:37 +0100

    Update setups

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@ sudo snap install --classic code
 sudo apt install latexmk
 sudo apt-get install texlive-full
 sudo apt install firefox
-sudo apt install google-chrome-stable
 sudo snap install gimp
 sudo apt install vlc
 sudo apt install htop
 sudo apt install git
 sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt install google-chrome-stable
 sudo apt update
